2ba8a1d94bb056321ffe4778b95a1281bb512936
Faisal Masood <fmasood@redhat.com>
2019-01-26 22:12:54 +1100

    initial commit

---
+++ src/main/java/com/faisal/demo/easynotes/repository/NoteRepository.java
@@ -0,0 +1,14 @@
+package com.faisal.demo.easynotes.repository;
+
+import com.faisal.demo.easynotes.model.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by rajeevkumarsingh on 27/06/17.
+ */
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+
+}
